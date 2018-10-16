71b4eb75e6f5975e7dce68466f24d3da2e252ce8
Santeri Sydanmetsa <Santeri.Sydanmetsa@students.ittralee.ie>
2018-10-16 15:57:54 +0100

    .gitignore and socket.io added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express       = require('express')
 const path          = require('path')
 const cookieParser  = require('cookie-parser')
 const logger        = require('morgan')
+const io			= require('socket.io')
 
 // Database connection
 require('./app/models/db')
